57bf9100c31dd3b1b8d8f6f13b3ffa97147ef62c
XavierGerD <xaviergervais@gmail.com>
2021-01-13 14:04:50 -0500

    First round

--- ScreenTestAdafruit4383.ino
+++ ScreenTestAdafruit4383.ino
@@ -25,22 +25,27 @@
 Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
 
 byte currentMenu = 0;
+byte firstMenuItemIndex = 0;
+byte currentSelectorPosition = 0;
+
 byte selectedItemBackground = 0x1335;
 byte menuTextSize = 3;
 byte menuItemOffset = 45;
 
 void changeSetting(byte setting, byte value) {
   Serial.println("Changing setting");
+  navigateToSubmenu(0);
 }
 
 void navigateToSubmenu(byte target) {
   currentMenu = target;
-  arraySize = sizeof(allMenus[currentMenu]);
-  drawMenu(ST77XX_WHITE);
+  firstMenuItemIndex = 0;
+  currentSelectorPosition = 0;
 }
 
 void changeScale(int scale) {
   Serial.println("Changing scale");
+  navigateToSubmenu(0);
 }
 
 class Menu {
@@ -53,26 +58,43 @@ class Menu {
       this -> submenuTarget = submenuTarget;
       this -> onAction = onAction;
     }
+};
+#define settingsLength 3
+#define sensorLength 4
+#define scaleLength 13
 
+Menu settingsMenuItems[settingsLength] = {
+  Menu("Sensor Mode", 1, navigateToSubmenu),
+  Menu("Scale", 2, navigateToSubmenu),
+  Menu("Config", 2, navigateToSubmenu)
 };
 
-Menu settingsMenuItems[2] = {Menu("Scale", 1, navigateToSubmenu), Menu("Sensor Mode", 2, navigateToSubmenu)};
-Menu sensorModeMenuItems[3] = {Menu("Velocity", 0, changeSetting), Menu("Mod Wheel", 0, changeSetting), Menu("Octave Shift", 0, changeSetting)};
-Menu scaleMenuItems[8] = {
-  Menu("Chromatic", 0, changeScale),
-  Menu("Major", 0, changeScale),
-  Menu("Minor", 0, changeScale),
-  Menu("Pentatonic", 0, changeScale),
+Menu sensorModeMenuItems[sensorLength] = {
+  Menu("Velocity", 0, changeSetting),
+  Menu("Mod Wheel", 0, changeSetting),
+  Menu("Pitch Bend", 0, changeSetting),
+  Menu("Octave Shift", 0, changeSetting)
+};
+
+Menu scaleMenuItems[scaleLength] = {
+  Menu("Chromatic", 0, changeScale), 
+  Menu("Ionian", 0, changeScale),
+  Menu("Dorian", 0, changeScale),
+  Menu("Phryigian", 0, changeScale),
   Menu("Lydian", 0, changeScale),
   Menu("Mixolydian", 0, changeScale),
   Menu("Aeolian", 0, changeScale),
-  Menu("Whole Tone", 0, changeScale)
+  Menu("Locrian", 0, changeScale),
+  Menu("Melodic Minor", 0, changeScale),
+  Menu("Penta. Maj.", 0, changeScale),
+  Menu("Penta. Min.", 0, changeScale),
+  Menu("Wh. T. 2, 1", 0, changeScale),
+  Menu("Wh. T. 1, 2", 0, changeScale),
 };
 Menu* allMenus[3] = {settingsMenuItems, sensorModeMenuItems, scaleMenuItems};
+byte menuLengths[3] = {settingsLength, sensorLength, scaleLength};
 
-byte arraySize = sizeof(allMenus[currentMenu]);
-byte firstMenuItemIndex = 0;
-byte currentSelectorPosition = 0;
+byte arraySize = menuLengths[currentMenu];
 
 boolean lastButtonState1 = false;
 boolean newButtonState1 = false;
@@ -103,20 +125,29 @@ void drawBackground(uint16_t color) {
   tft.drawRect(-1, -1, 242, 137, color);
 }
 
+byte getIndexOfFirstShownElement() {
+  return firstMenuItemIndex + 2 <= arraySize ?  firstMenuItemIndex  : arraySize - 3;
+}
+
 void drawMenu(uint16_t color) {
   drawBackground(ST77XX_BLACK);
   drawSelector();
   byte offsetCounter = 0;
+  byte indexOfFirstShownElement = getIndexOfFirstShownElement();
 
-  byte startingPoint = firstMenuItemIndex + 2 <= arraySize ?  firstMenuItemIndex  : arraySize - 3;
-
-  for (byte i = startingPoint; i < arraySize; i++) {
+  for (byte i = indexOfFirstShownElement; i < arraySize; i++) {
     tft.setCursor(5, offsetCounter * menuItemOffset + 10);
     tft.setTextColor(color);
     tft.setTextSize(menuTextSize);
     tft.print(allMenus[currentMenu][i].menuName);
     offsetCounter++;
   }
+}
+
+byte getSelectedItemIndex() {
+  byte cursorPosition = currentSelectorPosition / menuItemOffset;
+  byte indexOfFirstShownElement = getIndexOfFirstShownElement();
+  return cursorPosition + indexOfFirstShownElement;
 }
 
 void loop() {
@@ -140,8 +171,13 @@ void loop() {
   newButtonState2 = digitalRead(4);
   if (newButtonState2 != lastButtonState2) {
     if (newButtonState2) {
-      Menu currentItem = allMenus[currentMenu][firstMenuItemIndex + 1];
+      Menu currentItem = allMenus[currentMenu][getSelectedItemIndex()];
+      //Serial.println(allMenus[currentMenu][getSelectedItemIndex()].menuName);
       currentItem.onAction(currentItem.submenuTarget);
+      arraySize = menuLengths[currentMenu];
+      Serial.println("target" + String(currentItem.submenuTarget));
+      Serial.println(arraySize);
+      drawMenu(ST77XX_WHITE);
     }
 
     lastButtonState2 = newButtonState2;
